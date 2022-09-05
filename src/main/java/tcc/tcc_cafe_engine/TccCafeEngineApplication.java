3362748d3906855406373795b5e79744669a7be0
pipe-felipe <alves.felipepipe@gmail.com>
2022-09-05 19:10:47 -0300

    Initial Commit

---
+++ src/main/java/tcc/tcc_cafe_engine/TccCafeEngineApplication.java
@@ -0,0 +1,13 @@
+package tcc.tcc_cafe_engine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TccCafeEngineApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TccCafeEngineApplication.class, args);
+    }
+
+}
---
+++ src/test/java/tcc/tcc_cafe_engine/TccCafeEngineApplicationTests.java
@@ -0,0 +1,13 @@
+package tcc.tcc_cafe_engine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TccCafeEngineApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
